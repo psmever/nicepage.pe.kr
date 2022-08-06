0c1fa288640e77063b3f0c72018b46aedfc9a4a5
psmever <psmever@gmail.com>
2022-08-06 17:57:17 +0900

    base mardown editor.

--- blog.frontend/next.config.js
+++ blog.frontend/next.config.js
@@ -1,13 +1,13 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-    reactStrictMode: true,
-    swcMinify: true,
-};
+// const nextConfig = {
+//     reactStrictMode: true,
+//     swcMinify: true,
+// };
 
 // ref: https://uiwjs.github.io/react-md-editor/#support-nextjs
 const removeImports = require('next-remove-imports')();
 module.exports = removeImports({
-    webpack(config, options) {
-        return config;
-    },
+    // webpack(config, options) {
+    //     return config;
+    // },
 });
