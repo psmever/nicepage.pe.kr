d3410621adaf96fcea5e4c71332f1fe10b5bff90
psmever <psmever@gmail.com>
2022-08-20 17:43:10 +0900

    site base data api.

--- service/app/Http/Services/SystemServices.php
+++ service/app/Http/Services/SystemServices.php
@@ -53,6 +53,41 @@ class SystemServices
      */
     public function getSiteData() : array
     {
-        return [];
+        $returnObject = function($codes) {
+            /**
+             * 공통 코드 그룹 별로 구분.
+             */
+            $code_group = array();
+            array_map(function($element) use (&$code_group) {
+
+                // FIXME : 아래 경고?
+                $code_group[$element['group_id']][] = [
+                    'code_id' => $element['code_id'],
+                    'code_name' => $element['code_name'],
+                ];
+
+            }, array_filter($codes, function($e) {
+                return $e['code_id'];
+            }));
+
+            /**
+             * 코드 명으로 분리.
+             */
+            $code_name = array();
+            array_map(function($element) use (&$code_name) {
+                $code_name[$element['code_id']] = $element['code_name'];
+            }, array_filter($codes, function($e) {
+                return $e['code_id'];
+            }));
+
+            return [
+                'code_name' => $code_name,
+                'code_group' => $code_group
+            ];
+        };
+
+        return [
+            'codes' => $returnObject($this->codeRepository->all()->toArray())
+        ];
     }
 }
