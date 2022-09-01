57755defb58545985c7878821060f54d166582fb
psmever <psmever@gmail.com>
2022-09-01 23:02:52 +0900

    로컬 마이그레이션.

---
+++ service/app/Console/Commands/Dev/ProdToDev.php
@@ -0,0 +1,131 @@
+<?php
+
+namespace App\Console\Commands\Dev;
+
+use App\Models\BlogPosts;
+use App\Models\BlogPostsThumbs;
+use App\Models\BlogPostTags;
+use App\Models\MediaFiles;
+use Illuminate\Console\Command;
+use Illuminate\Support\Facades\DB;
+
+class ProdToDev extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'dev:get-blog-data';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'get prod blog data';
+
+    /**
+     * Execute the console command.
+     *
+     * @return int
+     */
+    public function handle()
+    {
+        DB::statement('SET FOREIGN_KEY_CHECKS=0;');
+        $task = DB::connection('prod_mysql')->table("media_files")->get()->toArray();
+
+        MediaFiles::truncate();
+        foreach ($task as $element):
+            MediaFiles::create([
+                'dest_path' => $element->dest_path,
+                'file_name' => $element->file_name,
+                'original_name' => $element->original_name,
+                'file_type' => $element->file_type,
+                'file_size' => $element->file_size,
+                'file_extension' => $element->file_extension,
+                'created_at' => $element->created_at,
+                'updated_at' => $element->updated_at,
+            ]);
+        endforeach;
+
+        // post
+        BlogPosts::truncate();
+        $task = DB::connection('prod_mysql')->table("posts")->get()->toArray();
+        foreach ($task as $element):
+            BlogPosts::create([
+                'user_id' => $element->user_id,
+                'post_uuid' => $element->post_uuid,
+                'category' => 'A05010',
+                'title' => $element->title,
+                'slug_title' => $element->slug_title,
+                'contents' => $element->contents_text,
+                'post_publish' => $element->post_publish,
+                'post_active' => $element->post_active,
+                'view_count' => $element->view_count,
+                'created_at' => $element->created_at,
+                'updated_at' => $element->updated_at
+            ]);
+        endforeach;
+
+        $task = DB::connection('prod_mysql')->table("section_posts")->get()->toArray();
+        foreach ($task as $element):
+            $gubun = $element->gubun;
+
+            $category = "";
+
+            if($gubun == 'S07010') {
+                $category = "A05020";
+            } else if($gubun == 'S07020') {
+                $category = "A05030";
+            } else if($gubun == 'S07030') {
+                $category = "A05040";
+            }
+
+            BlogPosts::create([
+                'user_id' => $element->user_id,
+                'post_uuid' => $element->post_uuid,
+                'category' => $category,
+                'title' => $element->title,
+                'slug_title' => $element->post_uuid,
+                'contents' => $element->contents_text,
+                'post_publish' => $element->display_flag,
+                'post_active' => $element->active,
+                'view_count' => $element->view_count,
+                'created_at' => $element->created_at,
+                'updated_at' => $element->updated_at
+            ]);
+
+        endforeach;
+
+        BlogPostTags::truncate();
+        $task = DB::connection('prod_mysql')->table("posts_tags")->get()->toArray();
+        foreach ($task as $element):
+
+            BlogPostTags::create([
+                'post_id' => $element->post_id,
+                'tag' => $element->tag_id,
+                'created_at' => $element->created_at,
+                'updated_at' => $element->updated_at
+            ]);
+
+        endforeach;
+
+
+        BlogPostsThumbs::truncate();
+        $task = DB::connection('prod_mysql')->table("posts_thumbs")->get()->toArray();
+        foreach ($task as $element):
+
+            BlogPostsThumbs::create([
+                'post_id' => $element->post_id,
+                'media_file_id' => $element->media_file_id,
+                'created_at' => $element->created_at,
+                'updated_at' => $element->updated_at
+            ]);
+
+        endforeach;
+
+        DB::statement('SET FOREIGN_KEY_CHECKS=1;');
+        return 0;
+    }
+}
--- service/config/database.php
+++ service/config/database.php
@@ -63,6 +63,26 @@ return [
             ]) : [],
         ],
 
+        'prod_mysql' => [
+            'driver' => 'mysql',
+            'url' => env('DATABASE_URL'),
+            'host' => env('PROD_DB_HOST', '127.0.0.1'),
+            'port' => env('PROD_DB_PORT', '3306'),
+            'database' => env('PROD_DB_DATABASE', 'forge'),
+            'username' => env('PROD_DB_USERNAME', 'forge'),
+            'password' => env('PROD_DB_PASSWORD', ''),
+            'unix_socket' => env('DB_SOCKET', ''),
+            'charset' => 'utf8mb4',
+            'collation' => 'utf8mb4_unicode_ci',
+            'prefix' => '',
+            'prefix_indexes' => true,
+            'strict' => true,
+            'engine' => null,
+            'options' => extension_loaded('pdo_mysql') ? array_filter([
+                PDO::MYSQL_ATTR_SSL_CA => env('MYSQL_ATTR_SSL_CA'),
+            ]) : [],
+        ],
+
         'pgsql' => [
             'driver' => 'pgsql',
             'url' => env('DATABASE_URL'),
